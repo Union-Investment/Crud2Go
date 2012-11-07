e7ad9cd4569c70d19f453a172f51614809a16cf1
Carsten Mjartan <carsten.mjartan@codecentric.de>
2012-11-07 11:21:13 +0100

    Various changes to make the code compile correctly with Oracle JDK 7

--- eai-portal-webapp-administration/src/test/java/de/unioninvestment/eai/portal/portlet/crud/beans/J2EEConnectionPoolTest.java
+++ eai-portal-webapp-administration/src/test/java/de/unioninvestment/eai/portal/portlet/crud/beans/J2EEConnectionPoolTest.java
@@ -1,24 +1,25 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package de.unioninvestment.eai.portal.portlet.crud.beans;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
 import java.sql.Connection;
@@ -45,6 +46,7 @@ public class J2EEConnectionPoolTest {
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
+		reset(InitialContextFactoryMock.getMock());
 		System.setProperty("java.naming.factory.initial",
 				"de.unioninvestment.eai.portal.portlet.crud.beans.InitialContextFactoryMock");
 		pool = new J2EEConnectionPool("jdbc/{0}Ds", "my");
