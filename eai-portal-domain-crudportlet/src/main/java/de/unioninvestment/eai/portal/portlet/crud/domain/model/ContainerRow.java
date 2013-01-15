86189076470d5dad5ed254cc02b2292a5c25167f
Carsten Mjartan <carsten.mjartan@codecentric.de>
2013-01-15 17:45:06 +0100

    Fixed isNewItem()/isModified() on Database ContainerRow

--- eai-portal-domain-crudportlet/src/main/java/de/unioninvestment/eai/portal/portlet/crud/domain/model/ContainerRow.java
+++ eai-portal-domain-crudportlet/src/main/java/de/unioninvestment/eai/portal/portlet/crud/domain/model/ContainerRow.java
@@ -1,21 +1,21 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package de.unioninvestment.eai.portal.portlet.crud.domain.model;
 
 import java.util.Map;
@@ -133,14 +133,25 @@ public abstract class ContainerRow implements Cloneable {
 		}
 	}
 
+	/**
+	 * @return <code>true</code>, falls die Tabellenzeile modifiziert wurde
+	 */
 	public boolean isModified() {
 		return false;
 	}
 
+	/**
+	 * @return <code>true</code>, falls die Tabellenzeile eine neue
+	 *         ungespeicherte Zeile darstellt
+	 */
 	public boolean isNewItem() {
 		return false;
 	}
 
+	/**
+	 * @return <code>true</code>, falls die Tabellenzeile als gelöscht markiert
+	 *         wurde
+	 */
 	public boolean isDeleted() {
 		return false;
 	}
