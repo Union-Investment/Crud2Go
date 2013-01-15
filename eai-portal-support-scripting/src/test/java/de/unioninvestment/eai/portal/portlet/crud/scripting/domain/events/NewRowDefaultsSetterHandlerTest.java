0d8d8195ddc55d15d3f664be52a32abba4cc82b5
Carsten Mjartan <carsten.mjartan@codecentric.de>
2013-01-15 11:18:19 +0100

    Correction of UI-611 - Empty defaults do not work on required table columns

--- eai-portal-support-scripting/src/test/java/de/unioninvestment/eai/portal/portlet/crud/scripting/domain/events/NewRowDefaultsSetterHandlerTest.java
+++ eai-portal-support-scripting/src/test/java/de/unioninvestment/eai/portal/portlet/crud/scripting/domain/events/NewRowDefaultsSetterHandlerTest.java
@@ -1,26 +1,27 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package de.unioninvestment.eai.portal.portlet.crud.scripting.domain.events;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import groovy.lang.Closure;
 
@@ -89,15 +90,25 @@ public class NewRowDefaultsSetterHandlerTest {
 		when(sourceMock.findEditor(anyString())).thenReturn(editorSupportMock);
 		when(editorSupportMock.createFormatter(Timestamp.class, null))
 				.thenReturn(
-				propertyFormatterMock);
+						propertyFormatterMock);
 		when(propertyFormatterMock.format(any())).thenReturn("01.01.2011");
 	}
 
 	@Test
-	public void shouldSetDfaultValue() {
+	public void shouldSetDefaultValue() {
 		handler.onCreate(event);
 
 		// assertThat((String) rowMock.asMap().get("column1"), is("2"));
 		verify(rowMock).setText("column1", "2");
+	}
+
+	@Test
+	public void shouldNotSetDefaultOnEmptyValue() {
+		when(gString1.call(any())).thenReturn("");
+
+		handler.onCreate(event);
+
+		// assertThat((String) rowMock.asMap().get("column1"), is("2"));
+		verifyZeroInteractions(rowMock);
 	}
 }
