9430d7628150617b199b5ebb3fe301dbf1749412
Jan Malcomess <jan.malcomess@codecentric.de>
2013-02-08 16:26:40 +0100

    Added 	replaceEach 	capitalizeFully 	deleteWhitespaces

--- eai-portal-support-scripting/src/main/java/de/unioninvestment/eai/portal/portlet/crud/scripting/category/StringCategory.java
+++ eai-portal-support-scripting/src/main/java/de/unioninvestment/eai/portal/portlet/crud/scripting/category/StringCategory.java
@@ -22,6 +22,8 @@ import groovy.lang.GString;
 
 import java.text.MessageFormat;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.WordUtils;
 import org.codehaus.groovy.runtime.GStringImpl;
 
 /**
@@ -53,5 +55,78 @@ public class StringCategory {
 
 	public static GString toGString(String self) {
 		return new GStringImpl(new Object[0], new String[] { self });
+	}
+
+	/**
+	 * <p>
+	 * Replaces all occurrences of Strings within another String.
+	 * </p>
+	 * <p>
+	 * For details and examples see
+	 * {@link StringUtils#replaceEach(String, String[], String[])}.
+	 * </p>
+	 * <p>
+	 * <b>NOTE:</b> this method becomes obsolete when new groovy-version becomes
+	 * available (2.1+) because then
+	 * <code>String#collectReplacements(Closure)</code> should be used.
+	 * 
+	 * @param s
+	 *            text to search and replace in, no-op if null
+	 * @param searchList
+	 *            the Strings to search for, no-op if null
+	 * @param replacementList
+	 *            the Strings to replace them with, no-op if null
+	 * @return the text with any replacements processed, <code>null</code> if
+	 *         null String input
+	 * @throws IndexOutOfBoundsException
+	 *             if the lengths of the arrays are not the same (null is ok,
+	 *             and/or size 0)
+	 * @since 1.46
+	 * @author Jan Malcomess (codecentric AG)
+	 */
+	public static String replaceEach(String s, String[] searchList,
+			String[] replacementList) {
+		return StringUtils.replaceEach(s, searchList, replacementList);
+	}
+
+	/**
+	 * <p>
+	 * Converts all the whitespace separated words in a String into capitalized
+	 * words, that is each word is made up of a titlecase character and then a
+	 * series of lowercase characters.
+	 * </p>
+	 * <p>
+	 * For details and examples see {@link WordUtils#capitalizeFully(String)}.
+	 * </p>
+	 * 
+	 * @param s
+	 *            the String to capitalize, may be null
+	 * @return capitalized String, <code>null</code> if null String input
+	 * @since 1.46
+	 * @author Jan Malcomess (codecentric AG)
+	 */
+	public static String capitalizeFully(String s) {
+		return WordUtils.capitalizeFully(s);
+	}
+
+	/**
+	 * <p>
+	 * Deletes all whitespaces from a String as defined by
+	 * {@link Character#isWhitespace(char)}.
+	 * </p>
+	 * <p>
+	 * For details and examples see {@link StringUtils#deleteWhitespace(String)}
+	 * .
+	 * </p>
+	 * 
+	 * @param s
+	 *            the String to delete whitespace from, may be null
+	 * @return the String without whitespaces, <code>null</code> if null String
+	 *         input
+	 * @since 1.46
+	 * @author Jan Malcomess (codecentric AG)
+	 */
+	public static String deleteWhitespaces(String s) {
+		return StringUtils.deleteWhitespace(s);
 	}
 }
