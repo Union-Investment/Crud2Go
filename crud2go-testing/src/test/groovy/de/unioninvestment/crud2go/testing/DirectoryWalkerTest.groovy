d7629d044f7e056ef8be62d9aac0db1823ff677e
serhiy-yevtushenko <serhiy.yevtushenko@codecentric.de>
2014-07-22 14:06:51 +0200

    Debug Info

--- crud2go-testing/src/test/groovy/de/unioninvestment/crud2go/testing/DirectoryWalkerTest.groovy
+++ crud2go-testing/src/test/groovy/de/unioninvestment/crud2go/testing/DirectoryWalkerTest.groovy
@@ -7,6 +7,8 @@ public class DirectoryWalkerTest {
 
 	@Test
 	void testWalking(){
+		println new File(".").getAbsolutePath()
+		
 		def fileList = []
 		new DirectoryWalkerProvider("src/test/resources/de/unioninvestment/crud2go/testing").each{obj -> 
 			fileList << obj
